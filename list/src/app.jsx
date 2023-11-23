3f50710265bfe5010124271fdb536ba3b902251b
Juan2040751 <viafara.juan@correounivalle.edu.co>
2023-11-22 22:18:23 -0500

    list user card

--- list/src/app.jsx
+++ list/src/app.jsx
@@ -1,25 +1,53 @@
 import { useFetch } from "./useFetch";
-import "./app.css"
+import "./app.css";
 
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const cardUser = (user) => {
+  return (
+    <React.Fragment key={user.id}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        {user.username}
+        </Typography>
+        <Typography variant="h5" component="div">
+        {user.name}
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+      {user.phone}
+      </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" href={user.website.startsWith("http") ? user.website : "http://" + user.website} >Learn More</Button>
+      </CardActions>
+    </React.Fragment>
+  );
+};
 function App() {
-    const { data, loading, error, handleCancelRequest } = useFetch("https://jsonplaceholder.typicode.com/users")
-    return(
-        <div className="App" >
-            <h1>API users</h1>
-            <button onClick={handleCancelRequest}>
-                Cancel Request
-            </button>
-            <div className="card">
-                <ul>
-                    { error && <li>Error: {error}</li>}
-                    { loading && <li>Loading...</li>}
-                    { data?.map((user) => (
-                        <li key={user.id}> {user.name} </li>
-                    )) }
-                </ul>
-            </div>
-        </div>
-    );
+  const { data, loading, error, handleCancelRequest } = useFetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  console.log({ data });
+  return (
+    <div className="App">
+      <h1>API users</h1>
+      <button onClick={handleCancelRequest}>Cancel Request</button>
+      <div className="card">
+        <ul>
+          {error && <li>Error: {error}</li>}
+          {loading && <li>Loading...</li>}
+          {data && data?.map((user) => cardUser(user))}
+    
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
