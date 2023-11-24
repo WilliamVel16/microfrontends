6721e51871c71d778afa8c767268f6dfbd808a83
Juan2040751 <viafara.juan@correounivalle.edu.co>
2023-11-24 12:08:50 -0500

    API POST

--- list4/src/components/ApiC.vue
+++ list4/src/components/ApiC.vue
@@ -1,9 +1,54 @@
 <template>
   <div class="AppUsers">
-    <h1>hola</h1>
-    <div class="containerUsers">
-      
-    </div>
+    <h1>API Posts</h1>
+    <ul class="containerUsers">
+      <b-card
+        v-for="post in posts"
+        :key="post.id"
+        class="MuiPaper-root"
+        style="
+          min-width: 275px;
+          max-width: 300px;
+          background-color: #fff;
+          color: rgba(0, 0, 0, 0.87);
+          -webkit-transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+          transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+          border-radius: 4px;
+          box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+            0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+            0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+          overflow: hidden;
+          padding: 16px;
+        "
+      >
+        <b-card-title
+          style="
+            margin: 0;
+            font-family: Roboto;
+            font-weight: 400;
+            font-size: 1.5rem;
+            line-height: 1.334;
+            letter-spacing: 0em;
+          "
+          >{{ post.title }}</b-card-title
+        >
+        <div>
+          <b-card-text
+            style="
+              margin: 0;
+              font-family: Roboto;
+              font-weight: 400;
+              font-size: 1rem;
+              line-height: 1.5;
+              letter-spacing: 0.00938em;
+              color: rgba(0, 0, 0, 0.6);
+              margin-bottom: 12px;
+            "
+            >{{ post.body }}</b-card-text
+          >
+        </div>
+      </b-card>
+    </ul>
   </div>
 </template>
 
@@ -18,25 +63,13 @@ export default {
   created() {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
-      .then((json) => this.posts = json);
+      .then((json) => {
+        this.posts = json;
+        console.log(json);
+      });
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
