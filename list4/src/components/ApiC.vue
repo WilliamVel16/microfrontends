a58b9b87f4c6fe6bb589cdefea39fd56412511f3
DianaCadenaMoreno <diana.marcela.cadena@correounivalle.edu.co>
2023-11-25 00:00:36 -0500

    styles

--- list4/src/components/ApiC.vue
+++ list4/src/components/ApiC.vue
@@ -5,47 +5,11 @@
       <b-card
         v-for="post in posts"
         :key="post.id"
-        class="MuiPaper-root"
-        style="
-          min-width: 275px;
-          max-width: 300px;
-          background-color: #fff;
-          color: rgba(0, 0, 0, 0.87);
-          -webkit-transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-          transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-          border-radius: 4px;
-          box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
-            0px 1px 1px 0px rgba(0, 0, 0, 0.14),
-            0px 1px 3px 0px rgba(0, 0, 0, 0.12);
-          overflow: hidden;
-          padding: 16px;
-        "
+        class="postCard"
       >
-        <b-card-title
-          style="
-            margin: 0;
-            font-family: Roboto;
-            font-weight: 400;
-            font-size: 1.5rem;
-            line-height: 1.334;
-            letter-spacing: 0em;
-          "
-          >{{ post.title }}</b-card-title
-        >
-        <div>
-          <b-card-text
-            style="
-              margin: 0;
-              font-family: Roboto;
-              font-weight: 400;
-              font-size: 1rem;
-              line-height: 1.5;
-              letter-spacing: 0.00938em;
-              color: rgba(0, 0, 0, 0.6);
-              margin-bottom: 12px;
-            "
-            >{{ post.body }}</b-card-text
-          >
+        <b-card-title class="postTitle">{{ post.title }}</b-card-title>
+        <div class="postBody">
+          <b-card-text>{{ post.body }}</b-card-text>
         </div>
       </b-card>
     </ul>
@@ -71,5 +35,52 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.AppUsers {
+  text-align: center;
+  margin-top: 20px;
+}
+
+.containerUsers {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 20px;
+}
+
+.postCard {
+  min-width: 275px;
+  max-width: 300px;
+  background-color: #fff;
+  color: rgba(0, 0, 0, 0.87);
+  transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  border-radius: 4px;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+    0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+  overflow: hidden;
+  padding: 16px;
+  margin-bottom: 20px;
+}
+
+.postTitle {
+  margin: 0;
+  font-family: Roboto;
+  font-weight: 400;
+  font-size: 1.5rem;
+  line-height: 1.334;
+  letter-spacing: 0em;
+}
+
+.postBody {
+  margin: 0;
+  font-family: Roboto;
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 1.5;
+  letter-spacing: 0.00938em;
+  color: rgba(0, 0, 0, 0.6);
+  margin-bottom: 12px;
+}
+</style>
+
--- list5/src/root.component.tsx
+++ list5/src/root.component.tsx
@@ -2,9 +2,8 @@ import { useEffect, useState } from "react";
 import { storeTodo, ITodo } from "@wvelorg/store";
 
 export default function Root(props) {
-
   const [todos, setTodos] = useState<ITodo[]>([]);
-  
+
   useEffect(() => {
     const subscription = storeTodo.storeTodo$.subscribe(setTodos);
 
@@ -13,12 +12,53 @@ export default function Root(props) {
     };
   }, []);
 
-  return <section>{props.name} is mounted!
-    <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
-      ))}
-      
-    </ul>
-  </section>;
+  return (
+    <section style={containerStyle}>
+      <div style={cardStyle}>
+        <h2 style={headerStyle}>{props.name} is mounted!</h2>
+        <ul style={listStyle}>
+          {todos.map((todo) => (
+            <li key={todo.id} style={listItemStyle}>
+              {todo.text}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 }
+
+// Estilos en línea
+const containerStyle = {
+  display: "flex",
+  justifyContent: "center",
+  height: "40vh", // Altura al 100% del viewport
+  margin: "0", // Sin margen en la parte superior e inferior
+};
+
+const cardStyle = {
+  width: "600px",
+  backgroundColor: "#f0f0f0", // Color de fondo del "card"
+  padding: "20px",
+  borderRadius: "8px",
+  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", 
+  overflow: "auto"
+};
+
+const headerStyle = {
+  fontFamily: "Arial, sans-serif", // Cambia la fuente según tus preferencias
+  fontSize: "24px",
+  marginBottom: "16px",
+};
+
+const listStyle = {
+  listStyle: "none",
+  padding: "0",
+  margin: "0",
+};
+
+const listItemStyle = {
+  fontSize: "18px",
+  marginBottom: "8px",
+};
+
