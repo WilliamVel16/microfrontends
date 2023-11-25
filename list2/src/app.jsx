76f7b8c2ad96ef6022b283b4ee48cf85d01d7494
Alejandro Grijalba <cesar.grijalba@correounivalle.edu.co>
2023-11-25 07:55:21 -0500

    agregando estilos

--- list2/src/app.jsx
+++ list2/src/app.jsx
@@ -1,26 +1,31 @@
 import { useFetch } from "./useFetch";
-import "./app.css"
+import "./app.css";
 
 function App() {
-    const { data, loading, error, handleCancelRequest } = useFetch("https://jsonplaceholder.typicode.com/photos")
-    const albums = data ? data.slice(0, 10) : [];
-    return(
-        <div className="App-2" >
-            <h1>API albums</h1>
-            <button className="button-2" onClick={handleCancelRequest}>
-                Cancel Request
-            </button>
-            <div className="card">
-                <ul className="ul-card-2">
-                    { error && <li>Error: {error}</li>}
-                    { loading && <li>Loading...</li>}
-                    { albums.map((alb) => (
-                        <li className="li-card-2" key={alb.id}> {alb.title}</li>
-                    )) }
-                </ul>
-            </div>
-        </div>
-    );
+  const { data, loading, error, handleCancelRequest } = useFetch(
+    "https://jsonplaceholder.typicode.com/photos"
+  );
+  const albums = data ? data.slice(0, 10) : [];
+  return (
+    <div className="App-albums">
+      <h1>API albums</h1>
+      <button className="button-albums" onClick={handleCancelRequest}>
+        Cancel Request
+      </button>
+      <div className="card-albums">
+        <ul className="ul-card-albums">
+          {error && <li>Error: {error}</li>}
+          {loading && <li>Loading...</li>}
+          {albums.map((alb) => (
+            <li className="li-card-albums" key={alb.id}>
+              {" "}
+              {alb.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
